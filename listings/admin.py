5ad9f19b69d4079d4c89532e3d5ca9c1b7893370
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-26 18:11:00 +0000

    feat: Added search functionality to Listings and a restriction of the amount of items to show

--- listings/admin.py
+++ listings/admin.py
@@ -7,4 +7,6 @@ class ListingAdmin(admin.ModelAdmin):
   list_display_links = ('id', 'title')
   list_filter = ('realtor', )
   list_editable = ('is_published', )
+  search_fields = ('title', 'description', 'address', 'city', 'county', 'price', 'postcode')
+  list_per_page = 25
 admin.site.register(Listing, ListingAdmin)
