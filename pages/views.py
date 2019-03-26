dc1fe21610cfe47aebd445a29eb24564a3629506
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-26 19:25:33 +0000

    refact: Retrieved realtors & mvp realtor from db & passed to the template

--- pages/views.py
+++ pages/views.py
@@ -15,4 +15,11 @@ def index(request):
   return render(request, 'pages/index.html', context)
 
 def about(request):
-  return render(request, 'pages/about.html')
+  realtors = Realtors.objects.order_by('-hire_date')
+  mvp_realtors = Realtors.objects.all().filter(is_mvp=True)
+  context = {
+    'realtors': realtors,
+    'mvp_realtors': mvp_realtors
+  }
+  
+  return render(request, 'pages/about.html', context)
