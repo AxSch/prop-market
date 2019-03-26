ae28609aef062a924cd289089b6e4ae5141366e0
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-26 16:31:17 +0000

    chore: Added empty lines

--- listings/models.py
+++ listings/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from datetime import datetime
-# from realtors.model import Realtor
+from realtors.model import Realtor
+
 # Create your models here.
 class Listing(models.Model):
-  # realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
   title =  models.CharField(max_length=200)
   address =  models.CharField(max_length=200)
   city = models.CharField(max_length=100)
