a80dfc6ce849a7bc56baa7491bf7b3a40179bbbf
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 15:17:15 +0000

    refact: Removed login view as option for usage of django LoginView from the auth lib

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages, auth
+from django.contrib import messages
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
@@ -11,32 +12,27 @@ def register(request):
     password = request.POST['password']
     password2 = request.POST['password2']
 
-    if password == password2:
-      if User.objects.filter(username=username).exists():
-        messages.error(request, "That username is taken")
-        return redirect('register')
-      else:
-        if User.objects.filter(email=email).exists():
-          messages.error(request, "That email is being used")
-          return redirect('register')
-        else:
-          user = User.objects.create_user(username=username,\
-            email=email, first_name=first_name, last_name=last_name)
-          user.save()
-          # auth.login(request, user)
-          messages.success(request, "You have been successfully registered!")
-          return redirect('login')
-    else:
-      messages.error(request, "Password do not match!")
+    if password != password2:
+      messages.error(request, 'Passwords do not match')
       return redirect('register')
+    
+    if User.objects.filter(username=username).exists():
+      messages.error(request, "That username is taken")
+      return redirect('register')
+    
+    if User.objects.filter(email=email).exists():
+      messages.error(request, "That email is being used")
+      return redirect('register')
+    
+    user = User.objects.create_user(username=username,\
+      email=email, first_name=first_name, last_name=last_name)
+    user.save()
+    # auth.login(request, user)
+    messages.success(request, "You have been successfully registered!")
+    return redirect('login')
   else:
     return render(request, 'accounts/register.html')
 
-def login(request):
-  if request.method == "POST":
-    pass  
-  else:
-    return render(request, 'accounts/login.html')
 
 def logout(request):
   return redirect('index')
