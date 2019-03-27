fca92dc8a39163360bece750edd51d91d77decad
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 17:06:29 +0000

    refact: Removed the template from the logout path

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
   path('login', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
   path('register', views.register, name='register'),
-  path('logout', auth_views.LogoutView.as_view(template_name='pages/index.html'), name='logout'),
+  path('logout', auth_views.LogoutView.as_view(), name='logout'),
   path('dashboard', views.dashboard, name='dashboard'),
 ]
