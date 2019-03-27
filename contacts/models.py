23e3d889963ef68387a01424a2c55c18888aca1d
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 18:48:57 +0000

    fix: Resolved duplicate model field & replaced

--- contacts/models.py
+++ contacts/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
   listing_id = models.IntegerField()
   name = models.CharField(max_length=200)
   email = models.CharField(max_length=100)
-  email = models.CharField(max_length=100)
+  phone = models.CharField(max_length=100, blank=True)
   message = models.TextField(blank=True)
   contact_date = models.DateTimeField(default=datetime.now, blank=True)
   user_id = models.IntegerField(blank=True)
