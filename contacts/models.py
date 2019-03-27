afd3a5cd3d4c32fe032e4172071614db5f7b92e1
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 19:34:52 +0000

    refact: Removed contact date and migrated(wip)

--- contacts/models.py
+++ contacts/models.py
@@ -8,7 +8,6 @@ class Contact(models.Model):
   email = models.CharField(max_length=100)
   phone = models.CharField(max_length=100, blank=True)
   message = models.TextField(blank=True)
-  contact_date = models.DateTimeField(default=datetime.now, blank=True)
   user_id = models.IntegerField(blank=True)
   def __str__(self):
     return self.name
