a9238f86bfcecee308bc47f24e61c7d0f36ef064
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 16:39:55 +0000

    refact: Swapped out views.logout for django auth LogoutView

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
   path('login', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
   path('register', views.register, name='register'),
-  path('logout', views.logout, name='logout'),
+  path('logout', auth_views.LogoutView.as_view(template_name='pages/index.html'), name='logout'),
   path('dashboard', views.dashboard, name='dashboard'),
 ]
