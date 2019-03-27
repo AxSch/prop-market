a24c8a756dfd03fefe4114475f95f4b738694d57
K.A. Schuneman <alex_schuneman@outlook.com>
2019-03-27 15:18:13 +0000

    refact: Removed views.login for django LoginView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-  path('login', views.login, name='login'),
+  path('login', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
   path('register', views.register, name='register'),
   path('logout', views.logout, name='logout'),
   path('dashboard', views.dashboard, name='dashboard'),
